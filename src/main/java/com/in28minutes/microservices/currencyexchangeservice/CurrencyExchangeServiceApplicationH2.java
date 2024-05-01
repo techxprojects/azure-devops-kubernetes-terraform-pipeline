74bb06be564a3dad03a97be3bb6dbe882af2317b
techxprojects <163107269+techxprojects@users.noreply.github.com>
2024-05-01 23:44:40 +0400

    Update CurrencyExchangeServiceApplicationH2 with error.java

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationH2.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationH2.java
@@ -9,5 +9,6 @@ public class CurrencyExchangeServiceApplicationH2 {
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeServiceApplicationH2.class, args);
 		// test comments 
+		abc comm
 	}
 }
